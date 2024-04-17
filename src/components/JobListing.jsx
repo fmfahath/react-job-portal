6cfc748507aa6e2a32025c73f0ff2324fc23fc67
fmfahath <fmfahath.dev@gmail.com>
2024-04-17 09:27:08 +0530

    JobListing : react-icons added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaMapMarker } from 'react-icons/fa';
 
 const JobListing = ({ job }) => {
     const [showFullDesc, setShowFullDesc] = useState(false);
@@ -31,7 +32,7 @@ const JobListing = ({ job }) => {
 
                 <div class="flex flex-col lg:flex-row justify-between mb-4">
                     <div class="text-orange-700 mb-3">
-                        <i class="fa-solid fa-location-dot text-lg"></i>
+                        <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
                     <a
