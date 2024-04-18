96142f71c307842d9c468b2b193bb4cf8f6d24fc
fmfahath <fmfahath.dev@gmail.com>
2024-04-18 21:07:12 +0530

    jobListings : api url proxy added (vite.config) -> not working

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -13,6 +13,7 @@ const JobListings = ({ isHome = false }) => {
         const fetchJobs = async () => {
 
             const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
+            // const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
 
             try {
                 const res = await fetch(apiUrl);
