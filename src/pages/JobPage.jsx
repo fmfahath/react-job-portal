405fca5179349833c0fa6f4778cde01fca4a4f75
fmfahath <fmfahath.dev@gmail.com>
2024-04-19 00:55:59 +0530

    jobPage : fetching data using fetch API

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import { FaArrowLeft } from 'react-icons/fa'
 
 const JobPage = () => {
+
+
+
+    const { id } = useParams();
+    const apiUrl = `http://localhost:8000/jobs/${id}`;
+
+    const [job, setJob] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const fetchJob = async () => {
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setJob(data);
+            } catch (error) {
+                console.log('fetch error: '.error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchJob();
+
+
+    }, []);
+
+
+
     return (
         <>
             <section>
@@ -23,9 +53,9 @@ const JobPage = () => {
                             <div
                                 className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
                             >
-                                <div className="text-gray-500 mb-4">Full-Time</div>
+                                <div className="text-gray-500 mb-4">{job.type}</div>
                                 <h1 className="text-3xl font-bold mb-4">
-                                    Senior React Developer
+                                    {job.title}
                                 </h1>
                                 <div
                                     className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
@@ -33,22 +63,22 @@ const JobPage = () => {
                                     <i
                                         className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
                                     ></i>
-                                    <p className="text-orange-700">Boston, MA</p>
+                                    <p className="text-orange-700">{job.location}</p>
                                 </div>
                             </div>
 
                             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                                 <h3 className="text-indigo-800 text-lg font-bold mb-6">
-                                    Job Description
+                                    Description
                                 </h3>
 
                                 <p className="mb-4">
-                                    We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
+                                    {job.description}
                                 </p>
 
                                 <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
 
-                                <p className="mb-4">$70k - $80K / Year</p>
+                                <p className="mb-4">{job.salary} / Year</p>
                             </div>
                         </main>
 
@@ -56,10 +86,12 @@ const JobPage = () => {
                             <div className="bg-white p-6 rounded-lg shadow-md">
                                 <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-                                <h2 className="text-2xl">NewTek Solutions</h2>
+                                <h2 className="text-2xl">Company Name</h2>
+                                {/* <h2 className="text-2xl">{job.company.name}</h2> */}
 
                                 <p className="my-2">
-                                    NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.
+                                    {/* {job.company.description} */}
+                                    Job description
                                 </p>
 
                                 <hr className="my-4" />
