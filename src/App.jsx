eef8625e9f474ebcd374d2ac9e537ef4f189e3f6
fmfahath <fmfahath.dev@gmail.com>
2024-04-19 16:05:42 +0530

    EditJobPage.jsx : created, routed, data loader used, delete reques created

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MainLayout from './layouts/MainLayout'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, { jobLoader } from './pages/JobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
 
 
@@ -20,7 +21,7 @@ const App = () => {
       },
       body: JSON.stringify(newJob)
     });
-    // return;
+    return;
   };
 
   //delete job
@@ -30,12 +31,25 @@ const App = () => {
     });
   };
 
+  //update job
+  const updateJob = async (job) => {
+    const res = await fetch(`http://localhost:8000/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/edit-job/:id' element={<EditJobPage addUpdateJob={updateJob} />} loader={jobLoader} />
         <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
